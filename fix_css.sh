193ca473337bf9da28850cb9dff0cbb09efbd848
sokcrow <sokutsuko2@gmail.com>
2026-03-15 09:27:23 -0600

    Delete fix_css.sh

--- fix_css.sh
+++
@@ -1,4 +0,0 @@
-cat hoja_personaje.css | grep -A 10 '\.shop-item-details'
-cat hoja_personaje.css | grep -A 10 '\.shop-item-name'
-cat hoja_personaje.css | grep -A 10 '\.shop-item-meta'
-cat hoja_personaje.css | grep -A 10 '\.shop-item-possession'
