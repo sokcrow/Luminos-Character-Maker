3b1cefda4fc85811d4127a06e9b8420a00af5545
sokcrow <sokutsuko2@gmail.com>
2026-08-21 23:05:49 -0600

    Load class milestone progression assets

--- js/dm-player-dnd-studio-hotfix.js
+++ js/dm-player-dnd-studio-hotfix.js
@@ -1,6 +1,48 @@
 (function (global) {
   "use strict";
 
+  const doc = global.document;
+
+  function ensureClassMilestoneAssets() {
+    if (!doc) return;
+
+    if (!doc.getElementById("dm-player-class-milestones-style")) {
+      const link = doc.createElement("link");
+      link.id = "dm-player-class-milestones-style";
+      link.rel = "stylesheet";
+      link.href = "css/dm-player-class-milestones.css";
+      doc.head?.appendChild(link);
+    }
+
+    const loadUi = () => {
+      if (global.LuminousDmPlayerClassMilestones || doc.getElementById("dm-player-class-milestones-script")) return;
+      const script = doc.createElement("script");
+      script.id = "dm-player-class-milestones-script";
+      script.src = "js/dm-player-class-milestones.js";
+      script.async = false;
+      doc.head?.appendChild(script);
+    };
+
+    if (global.LuminousClassMilestones) {
+      loadUi();
+      return;
+    }
+
+    let engineScript = doc.getElementById("class-milestone-engine-script");
+    if (!engineScript) {
+      engineScript = doc.createElement("script");
+      engineScript.id = "class-milestone-engine-script";
+      engineScript.src = "js/class-milestone-engine.js";
+      engineScript.async = false;
+      engineScript.addEventListener("load", loadUi, { once: true });
+      doc.head?.appendChild(engineScript);
+    } else {
+      engineScript.addEventListener("load", loadUi, { once: true });
+    }
+  }
+
+  ensureClassMilestoneAssets();
+
   if (!global?.Node || global.LuminousDmPlayerDndObserverHotfix) return;
 
   const descriptor = Object.getOwnPropertyDescriptor(global.Node.prototype, "textContent");
