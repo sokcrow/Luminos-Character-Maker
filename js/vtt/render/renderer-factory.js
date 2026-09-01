87f5658ccca091a70ddf2e91f346a1e133f15e7a
sokcrow <sokutsuko2@gmail.com>
2026-09-01 14:26:15 -0600

    Rama 3 paso 3: install transient token previews on renderers

--- js/vtt/render/renderer-factory.js
+++ js/vtt/render/renderer-factory.js
@@ -1,6 +1,7 @@
 import { Canvas2DRenderer } from './canvas2d-renderer.js';
 import { installDmObserverOverlay } from './dm-observer-overlay.js';
 import { installPersistentTokenViews } from './persistent-token-views.js';
+import { installTransientTokenPreview } from './transient-token-preview.js';
 import { RENDERER_BACKENDS } from './renderer-backend.js';
 import { WebGL2Renderer } from './webgl2-renderer.js';
 
@@ -10,5 +11,6 @@ export function createRenderer(canvas, mapData, { backend = RENDERER_BACKENDS.CA
         : new Canvas2DRenderer(canvas, mapData);
 
     installPersistentTokenViews(renderer);
+    installTransientTokenPreview(renderer);
     return installDmObserverOverlay(renderer);
 }
