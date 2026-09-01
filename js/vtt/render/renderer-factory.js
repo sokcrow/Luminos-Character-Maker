bc3dcef6788bfa4ed83b9a29f7138275119ff373
sokcrow <sokutsuko2@gmail.com>
2026-08-31 23:15:40 -0600

    feat(vtt): add renderer factory

---
+++ js/vtt/render/renderer-factory.js
@@ -0,0 +1,10 @@
+import { Renderer as Canvas2DRenderer } from '../renderer.js';
+import { RENDERER_BACKENDS } from './renderer-backend.js';
+import { WebGL2Renderer } from './webgl2-renderer.js';
+
+export function createRenderer(canvas, mapData, { backend = RENDERER_BACKENDS.CANVAS_2D } = {}) {
+    if (backend === RENDERER_BACKENDS.WEBGL_2) {
+        return new WebGL2Renderer(canvas, mapData);
+    }
+    return new Canvas2DRenderer(canvas, mapData);
+}
