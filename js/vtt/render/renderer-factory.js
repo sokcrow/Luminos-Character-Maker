03190eaa430ec558f321287654ad31dd54a8f86e
sokcrow <sokutsuko2@gmail.com>
2026-09-01 08:03:35 -0600

    refactor(vtt): install backend-neutral DM observer overlay

--- js/vtt/render/renderer-factory.js
+++ js/vtt/render/renderer-factory.js
@@ -1,10 +1,12 @@
 import { Canvas2DRenderer } from './canvas2d-renderer.js';
+import { installDmObserverOverlay } from './dm-observer-overlay.js';
 import { RENDERER_BACKENDS } from './renderer-backend.js';
 import { WebGL2Renderer } from './webgl2-renderer.js';
 
 export function createRenderer(canvas, mapData, { backend = RENDERER_BACKENDS.CANVAS_2D } = {}) {
-    if (backend === RENDERER_BACKENDS.WEBGL_2) {
-        return new WebGL2Renderer(canvas, mapData);
-    }
-    return new Canvas2DRenderer(canvas, mapData);
+    const renderer = backend === RENDERER_BACKENDS.WEBGL_2
+        ? new WebGL2Renderer(canvas, mapData)
+        : new Canvas2DRenderer(canvas, mapData);
+
+    return installDmObserverOverlay(renderer);
 }
