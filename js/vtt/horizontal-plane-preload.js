a560fb7bdc27fc1b563e4d83b77e6540e0e44cda
sokcrow <sokutsuko2@gmail.com>
2026-09-02 08:04:07 -0600

    feat(vtt): preload mechanic test lab runtime

--- js/vtt/horizontal-plane-preload.js
+++ js/vtt/horizontal-plane-preload.js
@@ -6,5 +6,6 @@ import './structure-authoring-patch.js';
 import './horizontal-plane-core.js';
 import './horizontal-plane-authoring-patch.js';
 import './vertical-transition-animation-runtime.js';
+import './map-test-lab-runtime.js';
 
 globalThis.LuminousVttHorizontalPlaneAuthoringPatch?.install?.();
