a0419252ff8b5c8d956b8e7d6ceca8ec3fd2f4ea
sokcrow <sokutsuko2@gmail.com>
2026-09-09 16:08:36 -0600

    feat: bootstrap reusable thrown projectile visuals

--- js/status-engine.js
+++ js/status-engine.js
@@ -197,6 +197,7 @@
   }
 
   ensureCoreConditionRuntime();
+  if (global.document && !global.LuminousThrownProjectileSequenceRuntime) loadScript("thrown-projectile-sequence-runtime-script", "js/thrown-projectile-sequence-runtime.js");
   if (global.document && !global.LuminousConditionCombatBridge) loadScript("core-condition-combat-bridge-script", "js/core-condition-combat-bridge.js");
   if (global.document && !global.LuminousConditionTheatreBridge) loadScript("core-condition-theatre-bridge-script", "js/core-condition-theatre-bridge.js");
   if (global.document && !global.LuminousCharacterBuildRules) loadScript("character-build-rules-script", "js/character-build-rules.js");
