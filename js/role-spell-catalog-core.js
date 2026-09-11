d0854c6e39a46dfbfb56907b70aee780c99bad76
sokcrow <sokutsuko2@gmail.com>
2026-09-11 14:34:03 -0600

    feat: add Angelo theater spells to role catalog

--- js/role-spell-catalog-core.js
+++ js/role-spell-catalog-core.js
@@ -35,6 +35,87 @@
           "open_or_close_door_or_window", "alter_eyes"
         ])
       })
+    }),
+
+    mage_hand: Object.freeze({
+      id: "mage_hand", name: "Mage Hand", nombre: "Mano de Mago",
+      level: 0, spellLevel: 0, cantrip: true,
+      classIds: ["bard", "sorcerer", "warlock", "wizard"],
+      school: "conjuration", contexts: ["theater"], castingTime: "action",
+      rangeFeet: 30, duration: "1_minute",
+      theater: Object.freeze({
+        type: "spectral_hand", target: "spectral_hand",
+        canManipulateObject: true, canOpenUnlockedDoorOrContainer: true,
+        canMoveUnattendedObject: true, commandMoveFeet: 30, maxCarryLb: 10,
+        cannotAttack: true, cannotActivateMagicItems: true
+      })
+    }),
+
+    prestidigitation: Object.freeze({
+      id: "prestidigitation", name: "Prestidigitation", nombre: "Prestidigitación",
+      level: 0, spellLevel: 0, cantrip: true,
+      classIds: ["artificer", "bard", "sorcerer", "warlock", "wizard"],
+      school: "transmutation", contexts: ["theater"], castingTime: "action",
+      rangeFeet: 10, duration: "up_to_1_hour",
+      theater: Object.freeze({
+        type: "minor_magic",
+        options: Object.freeze([
+          "harmless_sensory_effect", "light_or_extinguish_small_flame", "clean_or_soil_object",
+          "chill_warm_or_flavor_nonliving_material", "add_color_mark_or_symbol", "create_small_trinket_or_minor_illusion"
+        ]),
+        combatPower: false, dealsDamage: false
+      })
+    }),
+
+    distort_value: Object.freeze({
+      id: "distort_value", name: "Distort Value", nombre: "Distorsionar el Valor",
+      level: 1, spellLevel: 1, cantrip: false,
+      classIds: ["bard", "sorcerer", "warlock", "wizard"],
+      school: "illusion", contexts: ["theater"], castingTime: "1_minute",
+      targetType: "touch", duration: "8_hours",
+      theater: Object.freeze({
+        type: "perceived_value", maxObjectSideFeet: 1,
+        options: Object.freeze({
+          increase: Object.freeze({ multiplier: 2, presentation: "desirable_details" }),
+          decrease: Object.freeze({ multiplier: 0.5, presentation: "undesirable_details" })
+        }),
+        combatPower: false
+      })
+    }),
+
+    comprehend_languages: Object.freeze({
+      id: "comprehend_languages", name: "Comprehend Languages", nombre: "Entender Idiomas",
+      level: 1, spellLevel: 1, cantrip: false,
+      classIds: ["bard", "sorcerer", "warlock", "wizard"],
+      school: "divination", contexts: ["theater"], castingTime: "action", ritual: true,
+      targetType: "self", duration: "1_hour",
+      theater: Object.freeze({
+        type: "temporary_language_access",
+        temporaryLanguageAccess: Object.freeze({
+          mode: "understand", scope: Object.freeze(["standard", "exotic"]),
+          duration: "1_hour", grantsSpeaking: false
+        }),
+        writtenTextRequiresTouch: true, approximateMinutesPerPage: 1,
+        excludesSpecialLanguages: Object.freeze(["distortion", "singularity", "special"])
+      })
+    }),
+
+    speak_with_animals: Object.freeze({
+      id: "speak_with_animals", name: "Speak with Animals", nombre: "Hablar con los Animales",
+      level: 1, spellLevel: 1, cantrip: false,
+      classIds: ["bard", "druid", "ranger"],
+      school: "divination", contexts: ["theater"], castingTime: "action", ritual: true,
+      targetType: "self", duration: "10_minutes",
+      theater: Object.freeze({
+        type: "temporary_communication",
+        temporaryCommunication: Object.freeze({
+          targetCreatureType: "beast", understandsTarget: true,
+          targetUnderstandsCaster: true, duration: "10_minutes"
+        }),
+        beastKnowledgeWindowHours: 24,
+        mayAttemptSmallFavorCheck: true,
+        grantsLanguage: false, permanentModification: false
+      })
     })
   });
 });
