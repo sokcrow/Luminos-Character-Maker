d6794f5962378a4b946381d02f40d19d8ff7676a
sokcrow <sokutsuko2@gmail.com>
2026-09-15 12:48:53 -0600

    test(items): cover weapon icon families

--- tests/item-icon-registry-smoke.cjs
+++ tests/item-icon-registry-smoke.cjs
@@ -8,18 +8,9 @@ const { pathToFileURL } = require('node:url');
   const registry = globalThis.LuminousItemIconRegistry;
 
   assert.ok(registry);
-  assert.equal(registry.VERSION, 14);
+  assert.equal(registry.VERSION, 15);
   assert.equal(registry.DEFAULT_GROUP, 'generic_item');
-  assert.equal(Object.keys(registry.GROUPS).length, 101);
-  assert.equal(registry.has('healing_hp'), true);
-  assert.equal(registry.has('weapon_melee'), true);
-  assert.equal(registry.has('structural_stock'), true);
-  assert.equal(registry.has('fasteners_hardware'), true);
-  assert.equal(registry.has('wire_cable'), true);
-  assert.equal(registry.has('glass_component'), true);
-  assert.equal(registry.has('container'), true);
-  assert.equal(registry.has('trap'), false);
-  assert.equal(registry.has('currency'), false);
+  assert.equal(Object.keys(registry.GROUPS).length, 121);
 
   const groups = registry.list();
   assert.equal(new Set(groups.map((entry) => entry.id)).size, groups.length);
@@ -29,56 +20,97 @@ const { pathToFileURL } = require('node:url');
     assert.match(entry.icon, /^https:\/\/imgur\.com\/[A-Za-z0-9]+\.png$/);
   }
 
-  assert.deepEqual(registry.list({ domain: 'equipment' }).map((entry) => entry.id), ['weapon_melee', 'weapon_ranged', 'shield', 'accessory', 'armor_light', 'armor_medium', 'armor_heavy']);
-  assert.deepEqual(registry.list({ domain: 'material' }).filter((entry) => entry.id.startsWith('meat_')).map((entry) => entry.id), ['meat_mammal', 'meat_bird', 'meat_fish', 'meat_shellfish', 'meat_reptile', 'meat_draconic', 'meat_insectoid']);
+  const legacyCritical = [
+    'healing_hp', 'food', 'meat_mammal', 'hide_mammal', 'hard_bone', 'scale_reptile',
+    'feather_raw', 'organ_internal', 'blood', 'venom_raw', 'ooze_gel', 'essence_raw',
+    'energy_core', 'fruit_raw', 'vegetable_raw', 'grain_seed_raw', 'spice_herb_raw',
+    'medicinal_herb_raw', 'fungus_raw', 'botanical_extract_raw', 'ore_raw', 'metal_ingot',
+    'gem_rough', 'gem_cut', 'structural_stock', 'fasteners_hardware', 'wire_cable',
+    'glass_component', 'container', 'tool', 'repair_kit', 'weapon_melee', 'weapon_ranged'
+  ];
+  for (const id of legacyCritical) assert.equal(registry.has(id), true, id);
 
-  const botanicalIds = ['fruit_raw', 'vegetable_raw', 'grain_seed_raw', 'spice_herb_raw', 'medicinal_herb_raw', 'fungus_raw', 'botanical_extract_raw'];
-  assert.deepEqual(registry.list({ domain: 'material' }).filter((entry) => botanicalIds.includes(entry.id)).map((entry) => entry.id), botanicalIds);
-
-  const mineralIconIds = ['ore_raw', 'metal_ingot', 'gem_rough', 'gem_cut'];
-  assert.deepEqual(registry.list({ domain: 'material' }).filter((entry) => mineralIconIds.includes(entry.id)).map((entry) => entry.id), mineralIconIds);
-
-  const componentIconIds = ['structural_stock', 'fasteners_hardware', 'wire_cable', 'glass_component', 'container'];
-  assert.deepEqual(registry.list({ domain: 'material' }).filter((entry) => componentIconIds.includes(entry.id)).map((entry) => entry.id), componentIconIds);
-
-  const toolIconIds = ['tool', 'harvest_kit', 'cooking_tools', 'smithing_tools', 'fabrication_tools', 'textile_tools', 'medical_tools', 'technical_tools', 'lapidary_tools', 'chemical_tools', 'repair_kit'];
-  assert.deepEqual(
-    registry.list({ domain: 'utility' }).filter((entry) => toolIconIds.includes(entry.id)).map((entry) => entry.id),
-    ['repair_kit', 'tool', 'harvest_kit', 'cooking_tools', 'smithing_tools', 'fabrication_tools', 'textile_tools', 'medical_tools', 'technical_tools', 'lapidary_tools', 'chemical_tools']
-  );
-
-  const aliasExpectations = {
-    'Light Armor': 'armor_light', 'Melee Weapon': 'weapon_melee', food_meat: 'food', meat: 'meat_mammal', hide_leather: 'hide_mammal', bone_horn: 'hard_bone', fang: 'hard_claw', tusk: 'hard_horn', scale: 'scale_reptile', shell: 'shell_carapace', chitin: 'chitin_plate', feather: 'feather_raw', wool: 'animal_fiber_raw', raw_silk: 'silk_raw', ore: 'ore_raw', mineral: 'ore_raw', ore_mineral: 'ore_raw', raw_mineral: 'ore_raw', ingot: 'metal_ingot', refined_metal: 'metal_ingot', alloy: 'metal_ingot', rough_gem: 'gem_rough', raw_gem: 'gem_rough', cut_gem: 'gem_cut', polished_gem: 'gem_cut', heart: 'organ_internal', eye: 'organ_sensory', brain: 'organ_brain', gland: 'organ_gland', humanoid_blood: 'blood', exotic_fluid: 'ichor', venom: 'venom_raw', toxic_secretion: 'venom_raw', acid: 'acid_secretion', ink: 'ink_secretion', pheromone: 'bio_secretion', slime: 'ooze_gel', conductive_gel: 'ooze_gel', arcane_essence: 'essence_raw', mana_core: 'energy_core', fruit: 'fruit_raw', vegetables: 'vegetable_raw', grain: 'grain_seed_raw', legume: 'grain_seed_raw', nut: 'grain_seed_raw', spice: 'spice_herb_raw', culinary_herb: 'spice_herb_raw', medicinal_herb: 'medicinal_herb_raw', toxic_herb: 'medicinal_herb_raw', mushroom: 'fungus_raw', sap: 'botanical_extract_raw', resin: 'botanical_extract_raw', botanical_extract: 'botanical_extract_raw', processed_stock: 'structural_stock', structural_material: 'structural_stock', fasteners: 'fasteners_hardware', hardware: 'fasteners_hardware', wire: 'wire_cable', cable: 'wire_cable', processed_glass: 'glass_component', glassware: 'glass_component', vessel: 'container', generic_container: 'container', harvesting_tool: 'harvest_kit', harvesting_tools: 'harvest_kit', cooking_tool: 'cooking_tools', smithing_tool: 'smithing_tools', fabrication_tool: 'fabrication_tools', textile_tool: 'textile_tools', medical_tool: 'medical_tools', technical_tool: 'technical_tools', lapidary_tool: 'lapidary_tools', chemical_tool: 'chemical_tools'
+  const weaponIcons = {
+    weapon_sword: 'https://imgur.com/3AEGrWu.png',
+    weapon_dagger: 'https://imgur.com/K3d5UEA.png',
+    weapon_polearm: 'https://imgur.com/WWODflC.png',
+    weapon_hammer: 'https://imgur.com/WwAq8r.png',
+    weapon_firearm_shotgun: 'https://imgur.com/46tMIlQ.png',
+    weapon_sling: 'https://imgur.com/hmPPD07.png',
+    weapon_firearm_pistol: 'https://imgur.com/gJJgmqX.png',
+    weapon_net: 'https://imgur.com/T3u9Z3N.png',
+    weapon_firearm_revolver: 'https://imgur.com/GCEeR8H.png',
+    weapon_firearm_smg: 'https://imgur.com/L5FQuTA.png',
+    weapon_spear: 'https://imgur.com/DUGcpP5.png',
+    weapon_crossbow: 'https://imgur.com/83PWc1r.png',
+    weapon_pick: 'https://imgur.com/dTwA4cO.png',
+    weapon_whip: 'https://imgur.com/Nw8Mdnp.png',
+    weapon_blunt: 'https://imgur.com/XElPYSo.png',
+    weapon_blowgun: 'https://imgur.com/qkKi2DL.png',
+    weapon_firearm_rifle: 'https://imgur.com/aXXrcFQ.png',
+    weapon_staff: 'https://imgur.com/AChSLCZ.png',
+    weapon_axe: 'https://imgur.com/Fp9MJRw.png',
+    weapon_bow: 'https://imgur.com/PWnWMq1.png'
   };
-  for (const [alias, expected] of Object.entries(aliasExpectations)) assert.equal(registry.canonicalGroupId(alias), expected);
+  for (const [id, icon] of Object.entries(weaponIcons)) {
+    assert.equal(registry.has(id), true, id);
+    assert.equal(registry.resolveIcon(id), icon, id);
+    assert.equal(registry.get(id).domain, 'equipment');
+  }
 
-  const expectedIcons = {
-    healing_hp: 'https://imgur.com/GcnX53v.png', food: 'https://imgur.com/uFmUpuD.png', meat_mammal: 'https://imgur.com/GQAGWzK.png', hide_mammal: 'https://imgur.com/nK6vQIR.png', hard_bone: 'https://imgur.com/HrqeZ0a.png', scale_reptile: 'https://imgur.com/cJz55WQ.png', feather_raw: 'https://imgur.com/EPkBtW0.png', organ_internal: 'https://imgur.com/rXlXbOG.png', blood: 'https://imgur.com/7a75QU2.png', venom_raw: 'https://imgur.com/8dBvLD5.png', ooze_gel: 'https://imgur.com/91cMoMg.png', essence_raw: 'https://imgur.com/Yay5U5o.png', energy_core: 'https://imgur.com/KBSJN7y.png', fruit_raw: 'https://imgur.com/JA0yMwM.png', vegetable_raw: 'https://imgur.com/QWUZK4g.png', grain_seed_raw: 'https://imgur.com/Z0TuVti.png', spice_herb_raw: 'https://imgur.com/zvEvhjc.png', medicinal_herb_raw: 'https://imgur.com/FY3Sda7.png', fungus_raw: 'https://imgur.com/YRTeaK5.png', botanical_extract_raw: 'https://imgur.com/818zarC.png', ore_raw: 'https://imgur.com/xR1qk05.png', metal_ingot: 'https://imgur.com/BNTZ6FI.png', gem_rough: 'https://imgur.com/18tq0PQ.png', gem_cut: 'https://imgur.com/sDAhiUI.png', structural_stock: 'https://imgur.com/C985Ijj.png', fasteners_hardware: 'https://imgur.com/UvxjuRG.png', wire_cable: 'https://imgur.com/NHMef8W.png', glass_component: 'https://imgur.com/s4C3Jog.png', container: 'https://imgur.com/DvIOvQk.png', harvest_kit: 'https://imgur.com/o1ZbzmZ.png', cooking_tools: 'https://imgur.com/9J5aoNo.png', smithing_tools: 'https://imgur.com/U9eG3Iw.png', fabrication_tools: 'https://imgur.com/KBJtJvx.png', textile_tools: 'https://imgur.com/GroyHFc.png', medical_tools: 'https://imgur.com/q0qnACw.png', technical_tools: 'https://imgur.com/oPFjjqC.png', lapidary_tools: 'https://imgur.com/EWDbp22.png', chemical_tools: 'https://imgur.com/jlcBHuz.png', toxin_material: 'https://imgur.com/8dBvLD5.png'
+  const equipmentIds = registry.list({ domain: 'equipment' }).map((entry) => entry.id);
+  assert.equal(equipmentIds.length, 27);
+  assert.deepEqual(equipmentIds.slice(0, 22), [
+    'weapon_melee', 'weapon_ranged',
+    'weapon_sword', 'weapon_dagger', 'weapon_polearm', 'weapon_hammer',
+    'weapon_firearm_shotgun', 'weapon_sling', 'weapon_firearm_pistol', 'weapon_net',
+    'weapon_firearm_revolver', 'weapon_firearm_smg', 'weapon_spear', 'weapon_crossbow',
+    'weapon_pick', 'weapon_whip', 'weapon_blunt', 'weapon_blowgun', 'weapon_firearm_rifle',
+    'weapon_staff', 'weapon_axe', 'weapon_bow'
+  ]);
+  assert.deepEqual(equipmentIds.slice(22), ['shield', 'accessory', 'armor_light', 'armor_medium', 'armor_heavy']);
+
+  const weaponAliasExpectations = {
+    sword: 'weapon_sword', dagger: 'weapon_dagger', polearm: 'weapon_polearm', pole_arm: 'weapon_polearm',
+    hammer: 'weapon_hammer', shotgun: 'weapon_firearm_shotgun', sling: 'weapon_sling',
+    pistol: 'weapon_firearm_pistol', net: 'weapon_net', revolver: 'weapon_firearm_revolver',
+    smg: 'weapon_firearm_smg', spear: 'weapon_spear', crossbow: 'weapon_crossbow',
+    pick: 'weapon_pick', pickaxe: 'weapon_pick', whip: 'weapon_whip', blunt_weapon: 'weapon_blunt',
+    blowgun: 'weapon_blowgun', rifle: 'weapon_firearm_rifle', staff: 'weapon_staff', axe: 'weapon_axe', bow: 'weapon_bow'
   };
-  for (const [id, icon] of Object.entries(expectedIcons)) assert.equal(registry.resolveIcon(id), icon);
+  for (const [alias, expected] of Object.entries(weaponAliasExpectations)) assert.equal(registry.canonicalGroupId(alias), expected);
+
+  const legacyAliasExpectations = {
+    'Light Armor': 'armor_light', 'Melee Weapon': 'weapon_melee', food_meat: 'food', meat: 'meat_mammal',
+    hide_leather: 'hide_mammal', bone_horn: 'hard_bone', fang: 'hard_claw', tusk: 'hard_horn',
+    scale: 'scale_reptile', shell: 'shell_carapace', chitin: 'chitin_plate', feather: 'feather_raw',
+    wool: 'animal_fiber_raw', raw_silk: 'silk_raw', ore: 'ore_raw', ore_mineral: 'ore_raw',
+    ingot: 'metal_ingot', rough_gem: 'gem_rough', cut_gem: 'gem_cut', heart: 'organ_internal',
+    eye: 'organ_sensory', brain: 'organ_brain', gland: 'organ_gland', humanoid_blood: 'blood',
+    exotic_fluid: 'ichor', venom: 'venom_raw', acid: 'acid_secretion', ink: 'ink_secretion',
+    slime: 'ooze_gel', arcane_essence: 'essence_raw', mana_core: 'energy_core', fruit: 'fruit_raw',
+    vegetables: 'vegetable_raw', grain: 'grain_seed_raw', spice: 'spice_herb_raw', mushroom: 'fungus_raw',
+    resin: 'botanical_extract_raw', processed_stock: 'structural_stock', fasteners: 'fasteners_hardware',
+    wire: 'wire_cable', vessel: 'container', harvesting_tool: 'harvest_kit', smithing_tool: 'smithing_tools',
+    technical_tool: 'technical_tools', chemical_tool: 'chemical_tools'
+  };
+  for (const [alias, expected] of Object.entries(legacyAliasExpectations)) assert.equal(registry.canonicalGroupId(alias), expected);
 
   assert.equal(registry.resolveIcon('ore_mineral'), 'https://imgur.com/xR1qk05.png');
-  assert.equal(registry.resolveIcon('ingot'), 'https://imgur.com/BNTZ6FI.png');
-  assert.equal(registry.resolveIcon('rough_gem'), 'https://imgur.com/18tq0PQ.png');
-  assert.equal(registry.resolveIcon('cut_gem'), 'https://imgur.com/sDAhiUI.png');
   assert.equal(registry.resolveIcon('harvesting_tool'), 'https://imgur.com/o1ZbzmZ.png');
-  assert.equal(registry.resolveIcon('chemical_tool'), 'https://imgur.com/jlcBHuz.png');
-  assert.equal(registry.resolveIcon('fruit'), 'https://imgur.com/JA0yMwM.png');
-  assert.equal(registry.resolveIcon('container'), 'https://imgur.com/DvIOvQk.png');
   assert.equal(registry.resolveIcon('wire'), 'https://imgur.com/NHMef8W.png');
+  assert.equal(registry.resolveIcon('bow'), 'https://imgur.com/PWnWMq1.png');
 
-  // Unknown IDs that happen to exist on Object.prototype must remain unknown.
   assert.equal(registry.canonicalGroupId('constructor'), 'constructor');
   assert.equal(registry.has('constructor'), false);
   assert.equal(registry.get('constructor', { fallback: false }), null);
   assert.equal(registry.get('constructor').id, 'generic_item');
-
   assert.equal(registry.resolveIcon('nightshade'), registry.resolveIcon('generic_item'));
   assert.equal(registry.get('missing-family').id, 'generic_item');
   assert.equal(registry.get('missing-family', { fallback: false }), null);
   assert.equal(registry.resolveIcon('healing_hp', { iconOverride: 'https://example.test/custom.png' }), 'https://example.test/custom.png');
 
-  console.log(`Item icon registry smoke: OK (${groups.length} families)`);
+  console.log(`Item icon registry smoke: OK (${groups.length} families, 20 weapon-specific families)`);
 })().catch((error) => {
   console.error(error);
   process.exitCode = 1;
