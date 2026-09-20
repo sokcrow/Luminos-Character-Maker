f69dacbf78ee462036ee94e4e6a0e54832813b9f
sokcrow <sokutsuko2@gmail.com>
2026-09-19 19:40:59 -0600

    feat(economy): rebase culinary produce prices to salary standard

--- js/item-catalog-plant-produce.js
+++ js/item-catalog-plant-produce.js
@@ -9,6 +9,7 @@
   const VERSION = 1;
   const FAMILY = "plant_produce";
   const CURRENCY = "AHN";
+  const AHN_ECONOMY_SCALE = 5;
   const DEFAULT_QUALITY = "standard";
   const MEASURE = "market_unit";
   const RECIPES_IMPLEMENTED = false;
@@ -64,7 +65,7 @@
       sourceLine: "botanical_market",
       purchasable: true,
       currency: CURRENCY,
-      standardUnitValueAhn,
+      standardUnitValueAhn: Math.round(standardUnitValueAhn * AHN_ECONOMY_SCALE),
       measure: MEASURE,
       baseQuality: DEFAULT_QUALITY,
       qualitySystem: "universal",
