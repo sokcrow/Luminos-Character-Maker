4accf758851426d2178e859a084629cd071b8493
sokcrow <sokutsuko2@gmail.com>
2026-09-19 19:42:20 -0600

    feat(economy): rebase status cure prices

--- js/item-catalog-status-cure.js
+++ js/item-catalog-status-cure.js
@@ -10,6 +10,7 @@
   const VERSION = 2;
   const FAMILY = "status_cure";
   const CURRENCY = "AHN";
+  const PRICE_SCALE_BY_SOURCE = Object.freeze({ generic:5, workshop:3, specialist:2 });
   const TIERS = Object.freeze(["I", "II", "III", "IV", "V"]);
   const USE_TIMINGS = Object.freeze(["action", "quick_action", "off_combat"]);
   const ARCHETYPE_TIMINGS = Object.freeze(["action", "quick_action", "action", "off_combat"]);
@@ -160,7 +161,7 @@
       tier,
       purchasable: true,
       currency: CURRENCY,
-      priceAhn,
+      priceAhn: Math.round(priceAhn * (PRICE_SCALE_BY_SOURCE[sourceLine] || 2)),
       stackable: true,
       runtime: {
         actionCost,
